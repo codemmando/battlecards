4b351ab0a2dc8191885352d3e55482703f3e4f5c
codemmando <rene.mikkelsen@protonmail.com>
2025-03-19 17:25:11 +0100

    made battle function

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from warship import WarShip
 
 def main():
+
     warship = WarShip("Duke", 20, 2000, 4, 16, 32, 2)
+    warship2 = WarShip("Queen", 20, 2100, 2, 32, 100, 4)
 
     warship.get_stats()
+    warship2.get_stats()
+
+    warship.battle(2,warship2)
+    warship.battle(1,warship2)
+    warship.battle(0,warship2)
 
 main()
--- warship.py
+++ warship.py
@@ -3,19 +3,23 @@ class WarShip():
     
     def __init__(self, class_name, max_speed, range, main_guns = None, ssm = None, sam = None, ciws = None):
         self.class_name = class_name
-        self.max_speed = max_speed
-        self.range = range
-        self.main_guns = main_guns
-        self.ssm = ssm
-        self.sam = sam
-        self.ciws = ciws
+        self.stats = [max_speed, range, main_guns, ssm, sam, ciws]
 
     def get_stats(self):
-        print(f"Class: {self.class_name}")
+        print(f"\nClass: {self.class_name}")
         print(f"---------stats----------")
-        print(f"Max speed: {self.max_speed} knots")
-        print(f"Range: {self.range} nm")
-        print(f"Main guns: {self.main_guns}")
-        print(f"SSM: {self.ssm}")
-        print(f"SAM: {self.sam}")
-        print(f"CIWS: {self.ciws}")
+        print(f"Max speed: {self.stats[0]} knots")
+        print(f"Range: {self.stats[1]} nm")
+        print(f"Main guns: {self.stats[2]}")
+        print(f"SSM: {self.stats[3]}")
+        print(f"SAM: {self.stats[4]}")
+        print(f"CIWS: {self.stats[5]}\n")
+    
+    def battle(self, stat, other):
+        print(f"{self.class_name} is about to battle {other.class_name}\n")
+        if self.stats[stat] > other.stats[stat]:
+            print(f"{self.class_name} Wins\n") 
+        elif self.stats[stat] == other.stats[stat]:
+            print(f"Draw between {self.class_name} and {other.class_name}\n")
+        else:
+            print(f"Defeat {self.class_name} Lost\n")
